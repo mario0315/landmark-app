e6fa1cf05dd47750631ad830b9cf80c35320157b
spaceboy28 <gi.spaceboy35@gmail.com>
2022-05-20 17:12:19 -0500

    Initial Commit

---
+++ swift-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-demo
+//
+//  Created by Logan Nguyễn on 5/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-demo/swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_demoApp.swift
+//  swift-demo
+//
+//  Created by Logan Nguyễn on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
