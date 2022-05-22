3343dbde229c2ac54b170627610dadeb12b7bb2f
spaceboy28 <gi.spaceboy35@gmail.com>
2022-05-22 14:33:31 -0500

    Drew the Badge Symbol

---
+++ swift-demo/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  swift-demo
+//
+//  Created by Logan Nguyễn on 5/22/22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom);
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
