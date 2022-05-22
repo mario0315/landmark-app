9566d336f703b49b821b0a576d33e8dac2d28ce8
spaceboy28 <gi.spaceboy35@gmail.com>
2022-05-22 17:55:09 -0500

    Added customized view transitions

--- swift-demo/Views/Hikes/HikeView.swift
+++ swift-demo/Views/Hikes/HikeView.swift
@@ -7,6 +7,19 @@ A view displaying information about a hike, including an elevation graph.
 
 import SwiftUI
 
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        // AnyTransition.slide makes the graph move in and out from different sides
+        // AnyTransition.move makes the graph moe in and out from the same side
+        // AnyTransition.move(edge: .trailing);
+        
+        .asymmetric(
+            insertion: .move(edge: .trailing).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity)
+        )
+    }
+}
+
 struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
@@ -44,6 +57,7 @@ struct HikeView: View {
 
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade);
             }
         }
     }
