13ee20f7bfa5a501b6dcf054912181540afc310e
spaceboy28 <gi.spaceboy35@gmail.com>
2022-05-22 12:06:49 -0500

    Added Model Object to Views

--- swift-demo/swift_demo_App.swift
+++ swift-demo/swift_demo_App.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main // identifies app's entry point
 struct swift_demo_App: App {
+    @StateObject private var modelData = ModelData();
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData);
         }
     }
 }
