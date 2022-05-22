08f311b2948e312347585660134f4bbd042b4e10
spaceboy28 <gi.spaceboy35@gmail.com>
2022-05-21 20:46:44 -0500

    Composed detail View (s.6)

--- swift-demo/MapView.swift
+++ swift-demo/MapView.swift
@@ -2,13 +2,17 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    /**
+        @State attribute is used to establish a source of truth for data in the app
+        Allow the varuable to be updated in more than one view
+     */
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
         span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
     )
-
+    
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region) // Passing a binding/reference using $
     }
 }
 
