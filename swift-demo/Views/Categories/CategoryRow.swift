13062a10869465b94af2e8fee9c6cf96a684c3c3
spaceboy28 <gi.spaceboy35@gmail.com>
2022-05-27 22:17:22 -0500

    Created CategoryRow

---
+++ swift-demo/Views/Categories/CategoryRow.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryRow.swift
+//  swift-demo
+//
+//  Created by Logan Nguyễn on 5/27/22.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String;
+    var items: [Landmark];
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5);
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark);
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow(
+            categoryName: ModelData().landmarks[0].category.rawValue,
+            items: Array(ModelData().landmarks.prefix(4)) // prefix returns a subsequence of the parent array
+        )
+    }
+}
