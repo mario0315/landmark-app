c62591846a7c8de67b8834539a4302b51b7455f6
spaceboy28 <gi.spaceboy35@gmail.com>
2022-05-27 21:50:42 -0500

    Added categories group and CategoriesHome View

--- swift-demo/Views/LandMarks/LandmarkDetail.swift
+++ swift-demo/Views/LandMarks/LandmarkDetail.swift
@@ -61,7 +61,7 @@ struct LandmarkDetail_Previews: PreviewProvider {
     static let modelData = ModelData();
     
     static var previews: some View {
-        LandmarkDetail(landmark: modelData.landmarks[0])
+        LandmarkDetail(landmark: modelData.landmarks[3])
             .environmentObject(modelData)
     }
 }
