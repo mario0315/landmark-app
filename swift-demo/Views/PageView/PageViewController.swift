2a06742ea6d68fa0a9d20d60d8b8428c94c50060
spaceboy28 <gi.spaceboy35@gmail.com>
2022-05-29 15:21:14 -0500

    Create the View Controller's Data Source

--- swift-demo/Views/PageView/PageViewController.swift
+++ swift-demo/Views/PageView/PageViewController.swift
@@ -3,12 +3,21 @@ import UIKit
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
+    
+    /**
+     SwiftUI calls makeCoordinator before makeUIVIewController() so that we have access to the coordinator object when configuring our view controller
+     */
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self);
+    }
 
     // like a POST api
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
+        
+        pageViewController.dataSource = context.coordinator;
 
         return pageViewController
     }
@@ -16,6 +25,45 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     // like a PUT api
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+    }
+    
+    
+    /**
+     SwiftUI manages your UIViewControllerRepresentable type’s coordinator,
+     and provides it as part of the context when calling the methods you created above.
+     */
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController;
+        var controllers = [UIViewController]()
+        
+        init (_ pageViewController: PageViewController) {
+            parent = pageViewController;
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        /**
+         These two required methods, pageViewController, establish the relationships between view controllers,
+         so that you can swipe back and forth between them.
+         */
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController)
+            else {return nil};
+            
+            if (index == 0) {
+                return controllers.last;
+            }
+            return controllers[index-1];
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController)
+            else {return nil}
+            
+            if (index + 1 == controllers.count) {
+                return controllers.first;
+            }
+            return controllers[index+1];
+        }
     }
 }
